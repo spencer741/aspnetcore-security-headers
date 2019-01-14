c8fc8dc18d2244bf51989b8d091262b302bf91fa
spencer741 <35856129+spencer741@users.noreply.github.com>
2019-01-14 00:17:57 -0600

    Add files via upload

---
+++ src-spencer741/HstsMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+
+namespace AtmosphereCommon.Middleware.SecurityHeaders.Hsts
+{
+    public class HstsMiddleware
+    {
+        //The HSTS Policy is communicated by the server to the user agent via an HTTPS response header field named "Strict-Transport-Security"
+        private const string HeaderName = "Strict-Transport-Security";
+
+        private readonly RequestDelegate _next;
+        private readonly string _headerValue;
+
+        public HstsMiddleware(RequestDelegate next /*,IOptions<HstsOptions> options ... for future use if needed to extend functionality*/)
+        {
+            _next = next;
+            _headerValue = options.Value.BuildHeaderValue();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //HSTS can only be applied to secure requests according to spec
+            //there really is no point adding it to insecure ones since MiTM can just strip the header
+
+            //Checks to see if header already exists, if not, it adds it.
+            if (context.Request.IsHttps && !ContainsHstsHeader(context.Response))
+            {
+                context.Response.Headers.Add(HeaderName, _headerValue);
+            }
+            await _next(context);
+        }
+
+        private bool ContainsHstsHeader(HttpResponse response)
+        {
+            //Ordinal looks at raw bytes that represent each character.
+            return response.Headers.Any(h => h.Key.Equals(HeaderName, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ src-spencer741/HstsMiddlewareTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AtmosphereCommon.Middleware.SecurityHeaders.Hsts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+
+namespace Class_and_Field
+{
+    public class HstsMiddlewareTests
+    {
+        //Adds hsts header if not included. Doesn't add if hsts header is included
+        public async Task hstsScreening()
+        {
+            //Mock request junk....
+            RequestDelegate mockNext = (HttpContext ctx) =>
+            {
+                return Task.CompletedTask;
+            };
+            var options = Options.Create(new HstsOptions()
+            {
+                Duration = TimeSpan.FromHours(1)
+            });
+            var sut = new HstsMiddleware(mockNext, options); //He was using unit testing. SUT - System Under Test
+            var mockContext = new DefaultHttpContext();
+            mockContext.Request.Scheme = "https";
+            mockContext.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+
+            //Represents middleware execution
+            await sut.Invoke(mockContext);
+
+            //Invoke throws System.ArgumentException if it tries to add the header again
+        }
+    }
+}
